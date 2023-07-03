0f9130aa960629048cd293a56ffa520845215499
Renato Spakauskas <renatospaka@gmail.com>
2023-07-03 20:41:14 -0300

    chore: :white_check_mark: SetContext and Context usecases tests
    adding unit tests for SetContext and Context usecases

--- TESTING.md
+++ TESTING.md
@@ -1,3 +1,6 @@
+## Clearing the Cache
+go clean -cache
+
 ## Profiling the test coverage
 go test -coverprofile ./tests/scheduler.out ./...
 go tool cover -html=./tests/scheduler.out -o ./tests/scheduler.html
---
+++ scheduler/worker/core/usecase/tests/setContext_test.go
@@ -0,0 +1,20 @@
+package tests
+
+import (
+	"context"
+	"testing"
+
+	"github.com/renatospaka/scheduler/scheduler/worker/core/usecase"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetContext(t *testing.T) {
+	// using Worker Repository Mock to inject into the usecase
+	repo := new(workerRepositoryMock)
+	usecases := usecase.NewWorkerUsecase(repo)
+
+	usecases.SetContext(context.Background())
+	ctx := usecases.Context()
+	assert.NotNil(t, ctx)
+	assert.IsType(t, context.Background(), ctx)
+}
